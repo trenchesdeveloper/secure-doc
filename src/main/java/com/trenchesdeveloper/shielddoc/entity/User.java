e78f7359107471059ef59d2bcf49e666998fce87
trenchesdeveloper <opeyemisamuel222@gmail.com>
2025-11-14 09:12:43 +0100

    add the user entity and annotations

--- src/main/java/com/trenchesdeveloper/shielddoc/entity/User.java
+++ src/main/java/com/trenchesdeveloper/shielddoc/entity/User.java
@@ -1,14 +1,44 @@
 package com.trenchesdeveloper.shielddoc.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Entity
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer Id;
+@Table(name = "users")
+@JsonInclude(JsonInclude.Include.NON_DEFAULT)
+public class User extends Auditable {
+    @Column(unique = true, nullable = false, updatable = false)
+    private String userId;
+    private String firstName;
+    private String lastName;
+    @Column(unique = true, nullable = false)
+    private String email;
+    private Integer loginAttempts;
+    private LocalDateTime lastLogin;
+    private String phone;
+    private String bio;
+    private String imageUrl;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean enabled;
+    private boolean mfaEnabled;
+    @JsonIgnore
+    private String qrCodeSecret;
+    @Column(columnDefinition = "TEXT")
+    private String qiCodeImageUri;
+    private String roles; //TODO: CREATE roles class and map here
+
+
 }
